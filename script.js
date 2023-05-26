16735765376ba4fc804de5be022ed98569a7bc54
Medeiros2006 <vcostavitoriac@gmail.com>
2023-05-26 08:14:20 -0300

    at.

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 
 
 function notas(){
-    let Nome = window.prompt('Digite Seu Nome')
-
-    let n1 = Number(window.prompt('Digite Sua Primeira Nota:'))
-    let n2 = Number(window.prompt('Digite Sua Segunda Nota:'))
-    let n3 = Number(window.prompt('Digite Sua Terceira Nota:'))
+    let nome = document.getElementById('nome').value
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+    let n3 = parseInt(document.getElementById('n3').value)
     
     media = (n1+n2+n3)/3
 
-    if(media >= 6)
-    if(media==10)
-     alert("Parabéns! Você foi Aprovado");
-    else
-    document.getElementById('valor').innerHTML = `${Nome}, sua média foi ${+ media}, você foi Aprovado!`;
-   else
-   document.getElementById('valor').innerHTML = `${Nome} sua média foi ${+ media}, você foi Reprovado!` 
-
+    if(media >= 6){
+    document.getElementById('valor').innerHTML = `${nome}, sua média foi ${+ media}, você foi Aprovado!`
+    }
+   else{
+   document.getElementById('valor').innerHTML = `${nome}, sua média foi ${+ media}, você foi Reprovado!` 
+   }
 
 }
 
